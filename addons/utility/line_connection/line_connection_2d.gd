5d68b873a6a1b607daae92d451c09cd0f1d3cc87
gazinaft <gazinaft@gmail.com>
2023-05-25 17:00:42 +0300

    fix sigmoid

--- addons/utility/line_connection/line_connection_2d.gd
+++ addons/utility/line_connection/line_connection_2d.gd
@@ -25,6 +25,6 @@ func adjust_sigmoid():
 		sigmoid.rotation_angle = 90
 		sigmoid.sigmoid_scale.y /= 2
 	else:
-		sigmoid.rotation_angle = 180
-		sigmoid.sigmoid_scale.x /= 2
+		sigmoid.sigmoid_scale.y /= -2
+		
 
