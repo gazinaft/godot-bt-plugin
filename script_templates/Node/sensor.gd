02269279e3bcbba184c8c04f1520acef09a0816f
PickDown-py <sodapumpkin@icloud.com>
2023-05-27 14:21:49 +0300

    sensor

--- script_templates/Node/sensor.gd
+++ script_templates/Node/sensor.gd
@@ -1,7 +1,7 @@
 # meta-name: Sensor
 # meta-default: true
 # meta-space-indent: 4
-extends Sensor
+extends SensorLogic
 
 #Use BlackBoard to write relevant information for the AI
 func _init(blackboard: BlackBoard):
